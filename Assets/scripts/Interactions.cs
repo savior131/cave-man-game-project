9ba2bf68af86374aaa7bb0632d8dcc3a0b8a53e5
savior131 <aa8861336@gmail.com>
2024-02-15 10:14:48 +0200

    new scene added
    -added mena n3rmr
    -added new cave man with egyption skin
    -added the nile scene assets

---
+++ Assets/scripts/Interactions.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactions : MonoBehaviour
+{
+    [SerializeField] Animator anim;
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            anim.SetTrigger("Inspect");
+        }
+    }
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed;
+    Vector3 dir;
+    [SerializeField] float rotationSpeed;
+    [SerializeField] Transform character;
+    [SerializeField] Animator anim;
+    [SerializeField] CharacterController characterController;
+    [SerializeField] float gravity = -9.82f;
+    float velocity;
+    [SerializeField] float gravityMultiplaier = 3.0f;
+    private void Update()
+    {
+       // if (DialogueSystem.inDialogue) return; 
+        handleGravity();
+        handleMovment();
+    }
+    void handleMovment()
+    {
+        dir = new Vector3(Input.GetAxis("Horizontal"), dir.y, Input.GetAxis("Vertical"));
+        dir.Normalize();
+        dir = Quaternion.Euler(0, -45, 0) * dir;
+        anim.SetFloat("speed", new Vector2(dir.x ,dir.z).magnitude);
+        characterController.Move(dir * speed * Time.deltaTime);
+        if (new Vector2(dir.x, dir.z).magnitude > 0)
+        {
+            character.transform.rotation = Quaternion.Lerp(character.transform.rotation, Quaternion.LookRotation(new Vector3(dir.x ,0, dir.z)), rotationSpeed * Time.deltaTime);
+        }
+    }
+    void handleGravity()
+    {
+        if(characterController.isGrounded && velocity < 0.0f)
+        {
+            velocity = -1.0f;
+        }
+        else
+        {
+            velocity += gravity * gravityMultiplaier * Time.deltaTime; 
+        }
+        dir.y = velocity;
+    }
+}
